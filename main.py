d1f673d2afb9905f54300f4572ee4f4d96d80daa
Arom Gu <60910936+aromgu@users.noreply.github.com>
2026-06-30 20:00:28 +0900

    linter test

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import requests
+import sys
 
 
-
-import math, sys;
-
-class Example(   object ):
-    def __init__    ( self, bar ):
-      if bar : bar+=1;  bar=bar* bar   ; return bar
-      else:
-                    some_string = "foo"
-                    return (sys.path, some_string)
+class Example(object):
+    def __init__(self, bar):
+        if bar:
+            bar += 1
+            bar = bar * bar
+            return bar
+        else:
+            some_string = "foo"
+            return (sys.path, some_string)
